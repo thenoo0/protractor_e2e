6b33852115cdc60f78c9464553caeebe928dc8b4
Mateusz <mateuszlach0@gmail.com>
2022-08-20 14:22:53 +0200

    Add configuration for screenshot reporter

--- conf.js
+++ conf.js
@@ -1,14 +1,40 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+	dest: 'target/screenshots',
+	filename: 'my-report.html'
+});
+
 exports.config = {
 	directConnect: true,
 
 	capabiliteis: {
-		browserName: 'chrome'	
+		browserName: 'chrome'
 	},
 
 	framework: 'jasmine',
 	specs: ['./tests/calculator.spec.js'],
 
 	jasmineNodeOpts: {
-		defaultTimeoutInterval:6000
+		defaultTimeoutInterval: 6000
+	},
+
+	// Setup the report before any tests start
+	beforeLaunch: function () {
+		return new Promise(function (resolve) {
+			reporter.beforeLaunch(resolve);
+		});
+	},
+
+	// Assign the test reporter to each running instance
+	onPrepare: function () {
+		jasmine.getEnv().addReporter(reporter);
+	},
+
+	// Close the report after all tests finish
+	afterLaunch: function (exitCode) {
+		return new Promise(function (resolve) {
+			reporter.afterLaunch(resolve.bind(this, exitCode));
+		});
 	}
 }
