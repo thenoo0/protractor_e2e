83ea745124f3401224e635f485c12e058df675c3
Mateusz <mateuszlach0@gmail.com>
2022-08-22 21:23:07 +0200

    Add html reporter

--- conf.js
+++ conf.js
@@ -10,5 +10,64 @@ exports.config = {
 
 	jasmineNodeOpts: {
 		defaultTimeoutInterval: 6000
+	},
+
+	onPrepare: function () {
+		var jasmineReporters = require('jasmine-reporters');
+		jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+			consolidateAll: true,
+			savePath: './',
+			filePrefix: 'xmlresults'
+		}));
+
+		var fs = require('fs-extra');
+
+		fs.emptyDir('screenshots/', function (err) {
+			console.log(err);
+		});
+
+		jasmine.getEnv().addReporter({
+			specDone: function (result) {
+				if (result.status == 'failed') {
+					browser.getCapabilities().then(function (caps) {
+						var browserName = caps.get('browserName');
+
+						browser.takeScreenshot().then(function (png) {
+							var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName + '.png');
+							stream.write(new Buffer(png, 'base64'));
+							stream.end();
+						});
+					});
+				}
+			}
+		});
+	},
+
+	//HTMLReport called once tests are finished
+	onComplete: function () {
+		var browserName, browserVersion;
+		var capsPromise = browser.getCapabilities();
+
+		capsPromise.then(function (caps) {
+			browserName = caps.get('browserName');
+			browserVersion = caps.get('version');
+			platform = caps.get('platform');
+
+			var HTMLReport = require('protractor-html-reporter-2');
+
+			testConfig = {
+				reportTitle: 'Protractor Test Execution Report',
+				outputPath: './',
+				outputFilename: 'ProtractorTestReport',
+				screenshotPath: './screenshots',
+				testBrowser: browserName,
+				browserVersion: browserVersion,
+				modifiedSuiteName: false,
+				screenshotsOnlyOnFailure: true,
+				testPlatform: platform
+			};
+			new HTMLReport().from('xmlresults.xml', testConfig);
+		});
 	}
+
 }
